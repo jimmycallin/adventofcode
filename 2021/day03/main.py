4be4618768c63dda455235eb7f6dd0cfc2b42d22
Jimmy Callin <jimmy.callin@gmail.com>
2021-12-04 21:06:53 +0100

    day 3 part 2

--- 2021/day03/main.py
+++ 2021/day03/main.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from collections import Counter
 
 # 1
 
@@ -14,3 +15,27 @@ with open("input.txt") as f:
     print("1:", gamma * epsilon)
 
 # 2
+
+def sorted_most_common(s):
+    l = Counter(s).most_common()
+    return list(sorted(l, key=lambda z: (-z[1], z[0])))
+
+def o2(lines, position=0):
+    if len(lines) == 1:
+        return int(lines[0], 2)
+    most_commons = dict(sorted_most_common([n[position] for n in lines]))
+    if most_commons["0"] > most_commons["1"]:
+        return o2([n for n in lines if n[position] == "0"], position + 1)
+    return o2([n for n in lines if n[position] == "1"], position + 1)
+
+def co2(lines, position=0):
+    if len(lines) == 1:
+        return int(lines[0], 2)
+    most_commons = dict(sorted_most_common([n[position] for n in lines]))
+    if most_commons["0"] <= most_commons["1"]:
+        return co2([n for n in lines if n[position] == "0"], position + 1)
+    return co2([n for n in lines if n[position] == "1"], position + 1)
+
+with open("input.txt") as f:
+    lines = [s.strip() for s in f]
+    print("2:", o2(lines) * co2(lines))
